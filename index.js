802db2fa8f033eab35c9c4c7418edca099566f29
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2021-08-21 03:43:06 -0700

    Started learning backend

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express');
-
+const bd = require('body-parser');
+const cors = require('cors');
 const app = express();
-
 const port = 5000;
+
+
+app.use(cors());
+app.use(bd.urlencoded({
+    extended:false
+}))
+
+app.use(bd.json());
 
 
 app.get('/', (req, res) => {
     res.send('<center><br><br>Started learning backend technology<br>Ghulam Qadir</center>')
 })
 
+app.post('/signUp', (req, res) => {
+    // res.send("SignUp API")
+    console.log(req.body.education[1])
+})
 
 app.listen(port, () => {
     console.log('Server is Running!')
