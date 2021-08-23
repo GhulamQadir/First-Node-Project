1a1f40d27cc0c1839243ebb739f77e4b87199c0e
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2021-08-23 06:42:10 -0700

    Started learning backend

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bd = require('body-parser');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
 const port = 5000;
+let authModel = require('./authSchema');
+// const { response } = require('express');
 
 
 app.use(cors());
@@ -12,6 +15,19 @@ app.use(bd.urlencoded({
 
 app.use(bd.json());
 
+mongoose.connect('mongodb+srv://GhulamQadir:admin2356@cluster0.qt2bg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on("connected", () => {
+    console.log("Database connected")
+})
+
+mongoose.connection.on("error", () => {
+    console.log("Database not connected")
+})
 
 app.get('/', (req, res) => {
     res.send('<center><br><br>Started learning backend technology<br>Ghulam Qadir</center>')
@@ -19,9 +35,29 @@ app.get('/', (req, res) => {
 
 app.post('/signUp', (req, res) => {
     // res.send("SignUp API")
-    console.log(req.body.education[1])
+    // console.log(req.body.education[1])
+    let userCreate = new authModel({email: req.body.email, password: req.body.password})
+    userCreate.save()
+    .then((response) => {
+        // console.log(response, 'response success')
+        res.status(200).send({result: response, message: "Data stored successfully"})
+    })
+    .catch((err) => {
+        // console.log(err, 'err')
+        res.status(400).send({result: err.message, message: "Data not stored successfully"})
+    })
 })
 
 app.listen(port, () => {
     console.log('Server is Running!')
 })
+
+
+
+
+// "contact": 8121234566,
+    // "education": ["Hafiz", "School", "College"],
+    // "post": {
+    //     "postName": "post1",
+    //     "postData": "20-August"
+    // }
